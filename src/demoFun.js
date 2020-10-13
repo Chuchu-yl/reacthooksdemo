527b53446f997299a8d48f0d432297d38bcf6972
cxylizhichu <cxylizhichu@163.com>
2020-10-13 15:24:48 +0800

    reacthooks基础01

---
+++ src/demoFun.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+function DemoFun() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        click me
+      </button>
+    </div>
+  );
+}
+
+export default DemoFun;
---
+++ src/demoFun02.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+
+function DemoFun02() {
+  const [count, setCount] = useState();
+
+  return <div></div>;
+}
+export default DemoFun02;
